1b5445ad7572ce6fc32aef241820afe1ba16109a
Matheus <mf635097@gmail.com>
2023-03-20 09:08:45 -0300

    Config tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 export default function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
+    <div className="App flex h-full w-full flex-col content-center items-center justify-center bg-white selection:bg-black">
+      <div>
+        <h1 className="cursor-pointer text-3xl font-bold delay-75 duration-300 ease-in-out hover:text-slate-400">
+          Test
+        </h1>
+        <p className="font-medium text-gray-800">
+          Just a test description to initial project using tailwind
+        </p>
+      </div>
     </div>
   );
 }
