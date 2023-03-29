8fa16fdf28a3e08272149b980755de6eb226278d
Matheus <mf635097@gmail.com>
2023-03-29 09:11:13 -0300

    refactor: Remove component logic and replace with custom time hook
    This commit removes the logic from the component and replaces it with a custom time hook. The new hook provides a cleaner and more modular approach, allowing the component to be decoupled from the time logic. This results in improved maintainability and reusability of the code.

--- src/components/Pomodoro/index.tsx
+++ src/components/Pomodoro/index.tsx
@@ -1,85 +1,57 @@
-import { useState, useEffect, useCallback } from 'react';
-import { useInterval } from '../../hooks/use-interval';
-import { Button } from '../Button';
+import { useTimer } from '../../hooks/useTimer';
+import { DisplayButton } from '../DisplayButton';
 import { Timer } from '../Timer';
-import { times } from '../../times';
 
 export const Pomodoro = () => {
-  const [mainTime, setMainTime] = useState(times.pomodoroTime);
-  const [timeCoutingStatus, setTimeCountingSatus] = useState(false);
-  const [working, setWorking] = useState(false);
-  const [resting, setResting] = useState(false);
-  const [fullWorkingTime, setFullWorkingTime] = useState(0);
-  const [completedCycles, setCompletedCycles] = useState(0);
-  const [startCoutingStatus, setStartCoutingStatus] = useState(false);
+  const {
+    pomodoroTime,
+    shortRestTime,
+    longRestTime,
+    mainTime,
+    label,
+    restart,
+    startCoutingStatus,
+    timeCoutingStatus,
+    completedCycles,
+    working,
+    setMainTime,
+    setLabel,
+    setWorking,
+    startTime,
+    setTimeCountingSatus,
+  } = useTimer();
 
-  useInterval(
-    () => {
-      setMainTime(mainTime - 1);
-      if (working) setFullWorkingTime(fullWorkingTime + 1);
-    },
-    timeCoutingStatus ? 1000 : null,
-  );
-
-  const startTime = () => {
-    setTimeCountingSatus(true);
-    setWorking(true);
-    setResting(false);
-    setStartCoutingStatus(true);
-    setMainTime(times.pomodoroTime);
-  };
-
-  const configureToResting = useCallback(() => {
-    setTimeCountingSatus(true);
-    setWorking(false);
-    setResting(true);
-    setCompletedCycles(completedCycles + 1);
-
-    if ((completedCycles + 1) % times.cycles === 0) {
-      setMainTime(times.longRestTime);
-    } else {
-      setMainTime(times.shortsRestTime);
-    }
-  }, [completedCycles]);
-
-  useEffect(() => {
-    if (mainTime > 0) return;
-
-    if (working) {
-      configureToResting();
-    }
-
-    if (resting) startTime();
-  }, [mainTime, working, completedCycles, resting, configureToResting]);
-
-  const displayMainTime = () => {
-    setMainTime(times.pomodoroTime);
+  const displayPomodoroTime = () => {
+    setMainTime(pomodoroTime);
     setWorking(true);
     setTimeCountingSatus(false);
+    setLabel('pomodoroTime');
   };
   const displayShortBreakTime = () => {
-    setMainTime(times.shortsRestTime);
+    setMainTime(shortRestTime);
     setTimeCountingSatus(false);
+    setLabel('shortRestTime');
   };
   const displayLongBreakTime = () => {
-    setMainTime(times.longRestTime);
+    setMainTime(longRestTime);
     setTimeCountingSatus(false);
+    setLabel('longRestTime');
   };
 
   return (
     <div className="flex flex-col items-center">
       <div className="my-6 flex gap-4">
-        <Button text="Pomodoro" onClick={displayMainTime} />
-        <Button text="Short Break" onClick={displayShortBreakTime} />
-        <Button text="Long Break" onClick={displayLongBreakTime} />
+        <DisplayButton text="Pomodoro" onClick={displayPomodoroTime} />
+        <DisplayButton text="Short Break" onClick={displayShortBreakTime} />
+        <DisplayButton text="Long Break" onClick={displayLongBreakTime} />
       </div>
-      <Timer timer={mainTime} working={working} />
+      <Timer timer={mainTime} label={label} working={working} />
       <div className="my-4 flex gap-4">
         <button
           onClick={startTime}
           className="rounded-md border-2 p-1 text-lg font-semibold uppercase shadow-lg transition duration-300 ease-in-out hover:border-bluishPurple hover:text-bluishPurple hover:shadow-bluishPurple/30"
         >
-          start
+          {restart ? 'Restart' : 'Start'}
         </button>
         {startCoutingStatus && (
           <button
