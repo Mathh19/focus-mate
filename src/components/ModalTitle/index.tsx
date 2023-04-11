096709ae74fee094bfa479d3e2fec1cc99488c63
Matheus <mf635097@gmail.com>
2023-04-11 06:41:01 -0300

    Add spacing in ModalTitle

--- src/components/ModalTitle/index.tsx
+++ src/components/ModalTitle/index.tsx
@@ -2,7 +2,7 @@ import { ModalTitleProps } from './types';
 
 export const ModalTitle = ({ children }: ModalTitleProps) => {
   return (
-    <div className="flex items-center gap-1 text-xl font-semibold">
+    <div className="flex items-center gap-2 text-xl font-semibold">
       {children}
     </div>
   );
