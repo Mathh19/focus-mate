61a4818b379c16d6a44bac1b74cf3ab1ca2bb603
Matheus <mf635097@gmail.com>
2023-04-20 13:02:46 -0300

    Fixed Header layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import { Logo } from '../Logo';
 
 export const Header = () => {
   return (
-    <header className="fixed top-0 flex w-full items-center justify-between p-2">
+    <header className="flex w-full items-center justify-between p-2">
       <Logo />
     </header>
   );
