30e25f9252e11573e2164e1c579c42f96593a40e
Matheus <mf635097@gmail.com>
2023-04-20 13:35:18 -0300

    Made the menu component for all tasks
    This component is responsible for configuring all the tasks, allowing the user to be able to check as finished, delete all tasks, also being able to delete only those that were finished

---
+++ src/components/DropdownMenuTasks/index.tsx
@@ -0,0 +1,64 @@
+import { useContext, useState } from 'react';
+import { TasksContext } from '../../contexts/TasksContext/TasksContext';
+import { HiMenu, HiCheck } from 'react-icons/hi';
+import { IoMdClose } from 'react-icons/io';
+import { MdDelete } from 'react-icons/md';
+import { VscClearAll } from 'react-icons/vsc';
+
+export const DropdownMenuTasks = () => {
+  const [open, setOpen] = useState(false);
+  const { tasks, deleteTask, setFinished } = useContext(TasksContext);
+
+  const handleClick = (fn: () => void) => {
+    fn();
+    setOpen(false);
+  };
+
+  const handleClickDeleteAllTask = () => {
+    if (tasks.length > 0) {
+      const alert = window.confirm(
+        'Do you really want to delete all your tasks?',
+      );
+      alert && deleteTask();
+      setOpen(false);
+    }
+    setOpen(false);
+  };
+
+  return (
+    <div className="relative text-4xl">
+      {!open ? (
+        <HiMenu onClick={() => setOpen(!open)} className="cursor-pointer" />
+      ) : (
+        <IoMdClose onClick={() => setOpen(!open)} className="cursor-pointer" />
+      )}
+      {open && (
+        <div className="absolute right-0 z-50 w-80 rounded-md bg-darkBlue p-2 text-2xl text-bluishPurple-dark">
+          <div className="flex flex-col">
+            <button
+              onClick={() => handleClick(() => setFinished(true))}
+              className="flex items-center justify-between rounded-md px-2 py-1 duration-200 ease-in hover:bg-bluishPurple-dark hover:text-textColor"
+            >
+              Check all tasks
+              <HiCheck />
+            </button>
+            <button
+              onClick={() => handleClick(() => deleteTask(undefined, true))}
+              className="flex items-center justify-between rounded-md px-2 py-1 duration-200 ease-in hover:bg-bluishPurple-dark hover:text-textColor"
+            >
+              Delete finished tasks
+              <VscClearAll />
+            </button>
+            <button
+              onClick={handleClickDeleteAllTask}
+              className="flex items-center justify-between rounded-md px-2 py-1 duration-200 ease-in hover:bg-warningColor hover:text-textColor"
+            >
+              Delete all tasks
+              <MdDelete />
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
