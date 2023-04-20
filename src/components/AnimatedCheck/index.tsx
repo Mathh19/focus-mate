4f0dbdd0f2ee8b11dc15a2a7474fe33965ccdce6
Matheus <mf635097@gmail.com>
2023-04-20 13:01:01 -0300

    Fixed AnimatedCheck component color and logic

--- src/components/AnimatedCheck/index.tsx
+++ src/components/AnimatedCheck/index.tsx
@@ -1,15 +1,25 @@
-import { useState } from 'react';
+import { useContext } from 'react';
+import { TasksContext } from '../../contexts/TasksContext/TasksContext';
+import { AnimatedCheckProps } from './types';
 
-export const AnimatedCheck = () => {
-  const [check, setCheck] = useState(false);
+export const AnimatedCheck = ({ task }: AnimatedCheckProps) => {
+  const { setFinished } = useContext(TasksContext);
+
+  const handleCheck = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    setFinished(!task.finished, task);
+  };
+
   return (
     <div
-      onClick={() => setCheck(!check)}
+      role="button"
+      onClick={handleCheck}
+      aria-label="A button to check your task"
       className={`relative h-6 w-6 cursor-pointer rounded-full border-[2px] ${
-        !check ? 'border-textColor' : 'border-successColor'
+        !task.finished ? 'border-textColor' : 'border-successColor'
       }`}
     >
-      {check && (
+      {task.finished && (
         <>
           <span className="check bottom-[6px] right-[9px] w-[10px] rotate-45 animate-check-short-line"></span>
           <span className="check bottom-[10px] left-[5px] w-[22px] -rotate-45 animate-check-long-line"></span>
---
+++ src/components/AnimatedCheck/types.ts
@@ -0,0 +1,5 @@
+import { TaskProps } from '../../contexts/TasksContext/types';
+
+export type AnimatedCheckProps = {
+  task: TaskProps;
+};
