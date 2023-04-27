5e527f96f3838a5e41f7c24fb6e9f128820ef336
Matheus <mf635097@gmail.com>
2023-04-27 16:49:49 -0300

    Adjusted site logo and fixed minimally cropped image

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import { Logo } from '../Logo';
 
 export const Header = () => {
   return (
-    <header className="flex w-full items-center justify-between p-2">
+    <header className="flex w-full items-center justify-between px-4 py-3">
       <Logo />
     </header>
   );
--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,14 +2,15 @@ export const Logo = () => {
   return (
     <div
       onClick={() => location.reload()}
+      aria-label="The logo site"
       className="flex cursor-pointer items-center gap-2"
     >
       <img
-        className="h-[86px] w-[86px]"
+        className="h-16 w-16"
         src="/logo-site.svg"
-        alt="The logo site"
+        alt="An image of the website's logo"
       />
-      <p className="font-sans text-2xl font-bold">Focus Mate</p>
+      <span className=" font-sans text-2xl font-bold">Focus Mate</span>
     </div>
   );
 };
