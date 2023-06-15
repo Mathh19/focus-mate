bdc793ed1b7460976873c44d06ead0719faae7fc
Matheus <mf635097@gmail.com>
2023-06-15 20:05:30 -0300

    refactor: improve DisplayTimeButton component layout for responsive design

--- src/components/DisplayTimeButton/index.tsx
+++ src/components/DisplayTimeButton/index.tsx
@@ -8,7 +8,7 @@ export const DisplayTimeButton = ({
   return (
     <button
       onClick={onClick}
-      className={`rounded bg-bluishGray from-bluishPurple to-pink p-1 text-xl font-semibold transition duration-300 ease-in-out hover:bg-gradient-to-tr hover:shadow-shadowAllSides hover:shadow-vibrantPurple focus:bg-gradient-to-tr focus:shadow-shadowAllSides focus:shadow-vibrantPurple blueTheme:from-blueTheme blueTheme:to-blueTheme-800 blueTheme:hover:shadow-blueTheme/60 blueTheme:focus:shadow-blueTheme/50 dark:bg-darkTheme-dark dark:from-darkTheme dark:to-darkTheme-600 dark:hover:shadow-darkTheme/60 dark:focus:shadow-darkTheme/50 ${
+      className={`rounded bg-bluishGray from-bluishPurple to-pink p-2 text-xl font-semibold transition duration-300 ease-in-out hover:bg-gradient-to-tr hover:shadow-shadowAllSides hover:shadow-vibrantPurple focus:bg-gradient-to-tr focus:shadow-shadowAllSides focus:shadow-vibrantPurple blueTheme:from-blueTheme blueTheme:to-blueTheme-800 blueTheme:hover:shadow-blueTheme/60 blueTheme:focus:shadow-blueTheme/50 dark:bg-darkTheme-dark dark:from-darkTheme dark:to-darkTheme-600 dark:hover:shadow-darkTheme/60 dark:focus:shadow-darkTheme/50 max-[435px]:text-base ${
         focused && 'bg-gradient-to-tr'
       }`}
     >
