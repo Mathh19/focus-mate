cec3098da98ab505e6df724740d6a702b99db9e4
Matheus <mf635097@gmail.com>
2023-07-03 15:04:13 -0300

    refactor: Remove unused function

--- src/utils/getDayIndex.ts
+++
@@ -1,8 +0,0 @@
-import { WeeklyTasksProps } from '../shared-types/tasks';
-
-export const getDayIndex = (
-  currentDay: string,
-  weeklyTasks: WeeklyTasksProps[],
-) => {
-  return weeklyTasks.findIndex((item) => item.day === currentDay);
-};
