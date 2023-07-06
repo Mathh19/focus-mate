2ee4eb9099aac2d7aa89a3ed69e34bb84bbf3caa
Matheus <mf635097@gmail.com>
2023-07-06 12:35:44 -0300

    fix: Improve accessibility of ContainerTaskItem component

--- src/components/ContainerTaskItem/index.tsx
+++ src/components/ContainerTaskItem/index.tsx
@@ -29,37 +29,38 @@ export const ContainerTaskItem = ({
 
   return (
     <div>
-      <ul
-        ref={ulRef}
-        className={`relative mb-4 max-h-[289px] ${
-          !dynamicHeight && 'min-h-[288px]'
-        } space-y-3 overflow-y-auto overflow-x-hidden px-2 ${
-          hasScroll &&
-          isScrolledToTop &&
-          `after:pointer-events-none after:absolute after:bottom-0 after:h-4 after:w-full after:animate-fade-in-slowly after:bg-gradient-to-t ${
-            shadowEffectColor === 'backgroundColor'
-              ? 'after:from-backgroundColor'
-              : 'after:from-darkGray'
-          } after:to-transparent after:content-[''] ${
-            shadowEffectColor === 'backgroundColor'
-              ? 'after:dark:from-darkBackgroundColor'
-              : 'after:dark:from-darkTheme-950'
-          }`
-        }`}
-      >
-        {tasks.length === 0 && (
-          <span
-            className={`flex items-center justify-center px-2 text-2xl font-semibold text-purplishGray ${
-              !dynamicHeight && 'min-h-[288px]'
-            }`}
-          >
-            No Tasks...
-          </span>
-        )}
-        {tasks.map((task, index) => (
-          <TaskItem key={index} task={task} />
-        ))}
-      </ul>
+      {tasks.length === 0 ? (
+        <span
+          className={`flex items-center justify-center px-2 text-2xl font-semibold text-white/40 ${
+            !dynamicHeight && 'min-h-[288px]'
+          }`}
+        >
+          No Tasks...
+        </span>
+      ) : (
+        <ul
+          ref={ulRef}
+          className={`relative mb-4 max-h-[289px] ${
+            !dynamicHeight && 'min-h-[288px]'
+          } space-y-3 overflow-y-auto overflow-x-hidden px-2 ${
+            hasScroll &&
+            isScrolledToTop &&
+            `after:pointer-events-none after:absolute after:bottom-0 after:h-4 after:w-full after:animate-fade-in-slowly after:bg-gradient-to-t ${
+              shadowEffectColor === 'backgroundColor'
+                ? 'after:from-backgroundColor'
+                : 'after:from-darkGray'
+            } after:to-transparent after:content-[''] ${
+              shadowEffectColor === 'backgroundColor'
+                ? 'after:dark:from-darkBackgroundColor'
+                : 'after:dark:from-darkTheme-950'
+            }`
+          }`}
+        >
+          {tasks.map((task, index) => (
+            <TaskItem key={index} task={task} />
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
