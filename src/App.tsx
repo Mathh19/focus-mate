57fe8c856c2176ed0acdd572ffd8462e4fd8fdb4
Matheus <mf635097@gmail.com>
2023-10-16 13:47:57 -0300

    fix: remove component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Footer } from './components/Layout/Footer';
 import { Header } from './components/Layout/Header';
 import { Pomodoro } from './components/Pomodoro';
-import { MessageAlert } from './components/UI/MessageAlert';
 
 export default function App() {
   return (
     <div className="App">
-      <MessageAlert />
       <Header />
       <Pomodoro />
       <Footer />
