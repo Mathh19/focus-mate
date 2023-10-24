d4b199238863be7104018f9ea9847eb06dd696de
Matheus <mf635097@gmail.com>
2023-10-24 15:59:29 -0300

    feat: add dynamic logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,4 +1,16 @@
+import { useContext } from 'react';
+import { PomodoroContext } from '../../contexts/PomodoroContext/PomodoroContext';
+
 export const Logo = () => {
+  const { configPomodoro } = useContext(PomodoroContext);
+
+  const logoPath =
+    configPomodoro.theme === 'defaultTheme'
+      ? '/logo-site.svg'
+      : configPomodoro.theme === 'blueTheme'
+      ? '/logo-site-blue-theme.svg'
+      : '/logo-site-dark-theme.svg';
+
   return (
     <div
       onClick={() => location.reload()}
@@ -7,7 +19,7 @@ export const Logo = () => {
     >
       <img
         className="h-16 w-16"
-        src="/logo-site.svg"
+        src={logoPath}
         alt="An image of the website's logo"
       />
       <span className=" font-sans text-2xl font-bold">Focus Mate</span>
