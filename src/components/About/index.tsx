0a85554f55f31ebc5f16bacf4076b89e9b061555
Matheus <mf635097@gmail.com>
2023-11-08 20:29:28 -0300

    refactor: Component About use Modal

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { BsQuestionCircle } from 'react-icons/bs';
 import { about } from '../../content/about.json';
+import { Modal } from '../UI/Modal';
 
 export const About = () => {
   const [open, setOpen] = useState(false);
@@ -15,22 +16,20 @@ export const About = () => {
         >
           <BsQuestionCircle className="text-3xl transition duration-200 ease-in-out hover:text-bluishPurple focus:text-bluishPurple blueTheme:hover:text-blueTheme blueTheme:focus:text-blueTheme dark:hover:text-white dark:focus:text-white" />
         </button>
-        {open && (
-          <div
-            onClick={() => setOpen(false)}
-            className="fixed inset-0 z-50 flex min-h-screen animate-fade-in-slowly items-center justify-center bg-backgroundColor/60 px-4"
-          >
-            <div className="rounded-lg bg-darkGray p-4 font-semibold dark:bg-darkTheme-dark-grey">
-              <div className="flex max-h-[500px] max-w-[450px] flex-col items-center gap-3 overflow-y-auto rounded-lg text-center text-lg font-semibold">
-                <BsQuestionCircle className="text-7xl text-bluishPurple blueTheme:text-blueTheme dark:text-white max-[432px]:min-h-[72px] max-[432px]:min-w-[72px]" />
-                <h2 className="text-3xl font-bold text-bluishPurple blueTheme:text-blueTheme dark:text-white">
-                  What is the Pomodoro ?
-                </h2>
-                <p>{about}</p>
-              </div>
+        <Modal.Root isOpen={open} setOpen={() => setOpen(!open)}>
+          <Modal.Content>
+            <div
+              onClick={() => setOpen(false)}
+              className="flex max-w-[450px] flex-col items-center justify-center gap-2  text-center"
+            >
+              <BsQuestionCircle className="text-7xl text-bluishPurple blueTheme:text-blueTheme dark:text-white max-[432px]:min-h-[72px] max-[432px]:min-w-[72px]" />
+              <h2 className="text-3xl font-bold text-bluishPurple blueTheme:text-blueTheme dark:text-white">
+                What is the Pomodoro ?
+              </h2>
+              <p className="text-lg font-semibold">{about}</p>
             </div>
-          </div>
-        )}
+          </Modal.Content>
+        </Modal.Root>
       </div>
     </div>
   );
