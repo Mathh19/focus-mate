8788c15df38325607e5f6f2717d897b9f758ba4b
Matheus <mf635097@gmail.com>
2023-11-08 20:36:05 -0300

    fix: fix import

--- src/components/UI/Modal/ModalHeader.tsx
+++ src/components/UI/Modal/ModalHeader.tsx
@@ -1,6 +1,4 @@
-import { ModalContentProps } from './types';
-
-export const ModalHeader = ({ children }: ModalContentProps) => {
+export const ModalHeader = ({ children }: { children: React.ReactNode }) => {
   return (
     <header className="flex items-center justify-between border-b border-bluishGray p-4">
       {children}
