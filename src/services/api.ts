8dcc3f9fea25639700abfaa2c0fb83ad5cd47b34
Matheus <mf635097@gmail.com>
2023-12-06 13:05:56 -0300

    feat: config axios

---
+++ src/services/api.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get('auth_token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
