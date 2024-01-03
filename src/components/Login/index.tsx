9646027559590c67c6d88f9d6b78612a790f2af6
Matheus <mf635097@gmail.com>
2024-01-03 14:31:48 -0300

    feat: add Login component

---
+++ src/components/Login/index.tsx
@@ -0,0 +1,183 @@
+import { useState, useContext } from 'react';
+import { IoClose } from 'react-icons/io5';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { Modal } from '../UI/Modal';
+import { Input } from '../UI/Input';
+import { signUp, uploadAvatar } from '../../services/signUp';
+import { AuthContext } from '../../contexts/AuthContext/AuthContext';
+import { ErrorMessage } from '../UI/ErrorMessage';
+import { UploadAvatar } from '../UI/UploadAvatar';
+import { validateAvatar } from '../../utils/validateAvatar';
+
+const schema = z.object({
+  username: z
+    .string()
+    .min(2, 'Enter a valid username must have between 2 and 25 characters')
+    .max(25, 'Enter a valid username must have between 2 and 25 characters')
+    .optional()
+    .or(z.literal('')),
+  email: z.string().email('Please enter a valid email.'),
+  password: z.string().min(8, 'Your password must have at least 8 characters.'),
+});
+
+type FormProps = z.infer<typeof schema>;
+
+export const Login = () => {
+  const [open, setOpen] = useState(false);
+  const [createAcc, setCreateAcc] = useState(false);
+  const [contentAvatar, setContentAvatar] = useState<File | undefined>();
+  const [errorMessage, setErrorMessage] = useState('');
+  const { signIn } = useContext(AuthContext);
+  const {
+    handleSubmit,
+    getValues,
+    register,
+    formState: { errors },
+  } = useForm<FormProps>({
+    mode: 'all',
+    reValidateMode: 'onChange',
+    resolver: zodResolver(schema),
+    defaultValues: {
+      username: '',
+      email: '',
+      password: '',
+    },
+  });
+  const { username, email, password } = getValues();
+
+  const handleSubmitForm = (data: FormProps) => {
+    const { email, password, username } = data;
+
+    if (createAcc && contentAvatar) {
+      if (validateAvatar(contentAvatar) !== true) {
+        setErrorMessage(validateAvatar(contentAvatar) as string);
+        return;
+      }
+    }
+
+    if (createAcc && username) {
+      signUp({ email, password, username })
+        .then(() => {
+          signIn(email, password).then(async () => {
+            if (contentAvatar) {
+              const formData = new FormData();
+              formData.append('file', contentAvatar as File);
+              uploadAvatar(formData);
+            }
+          });
+          setOpen(false);
+        })
+        .catch((err) => setErrorMessage(err.response.data.message));
+      return;
+    }
+    signIn(email, password).catch((err) =>
+      setErrorMessage(err.response.data.message),
+    );
+  };
+
+  const handleOnChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement & {
+      files: File;
+    };
+    setContentAvatar(target.files[0]);
+  };
+
+  return (
+    <div>
+      <button
+        aria-label="Opens the option for you to log in or log out"
+        onClick={() => setOpen(true)}
+        className="rounded-sm bg-bluishPurple px-2 py-1 font-semibold outline-4 blueTheme:bg-blueTheme dark:bg-white dark:text-backgroundColor"
+      >
+        Login
+      </button>
+      <Modal.Root isOpen={open} setOpen={() => setOpen(false)}>
+        <Modal.Header>
+          <h2 className="text-xl font-semibold">
+            {createAcc ? 'Sign up' : 'Sign in'}
+          </h2>
+          <button
+            onClick={() => setOpen(false)}
+            aria-label="close modal"
+            className="text-2xl"
+          >
+            <IoClose />
+          </button>
+        </Modal.Header>
+        <Modal.Content>
+          <form
+            onSubmit={handleSubmit(handleSubmitForm)}
+            className="flex w-full max-w-xs flex-col items-center space-y-3"
+          >
+            {createAcc && (
+              <>
+                <UploadAvatar
+                  contentImage={contentAvatar}
+                  onChange={handleOnChange}
+                />
+                {contentAvatar && (
+                  <button
+                    onClick={() => setContentAvatar(undefined)}
+                    className="rounded-sm border-2 border-blueTheme-grey px-1.5"
+                  >
+                    remove avatar
+                  </button>
+                )}
+                <Input
+                  {...register('username')}
+                  error={errors.username && errors.username.message}
+                  name="username"
+                  label="username"
+                  type="text"
+                  autoComplete="username"
+                  placeholder="username"
+                  valueInput={username}
+                />
+              </>
+            )}
+            <Input
+              {...register('email')}
+              error={errors.email && errors.email.message}
+              label="Email"
+              type="email"
+              autoComplete="email"
+              placeholder="email@example.com"
+              valueInput={email}
+            />
+            <Input
+              {...register('password')}
+              error={errors.password && errors.password.message}
+              name="password"
+              label="Password"
+              type="password"
+              placeholder="passsword"
+              valueInput={password}
+            />
+            {errorMessage.length > 0 && <ErrorMessage error={errorMessage} />}
+            <div className="flex w-full flex-col gap-4 py-5">
+              <button
+                type="submit"
+                className="w-full rounded-sm bg-bluishPurple p-2 font-semibold blueTheme:bg-blueTheme dark:bg-white dark:text-darkBackgroundColor"
+              >
+                {createAcc ? 'register' : 'login'}
+              </button>
+              <p className="text-sm">
+                {!createAcc
+                  ? 'First time using Focus mate?'
+                  : 'Already part of Focus mate?'}{' '}
+                <span
+                  onClick={() => setCreateAcc(!createAcc)}
+                  className="cursor-pointer text-bluishPurple blueTheme:text-blueTheme dark:text-white"
+                >
+                  {!createAcc ? 'Register' : 'Login'}
+                </span>
+              </p>
+            </div>
+          </form>
+        </Modal.Content>
+      </Modal.Root>
+    </div>
+  );
+};
