6f670e5251e5c9e591016adbcf0850b7516c61d1
Matheus <mf635097@gmail.com>
2024-01-03 14:23:28 -0300

    feat: improve feedback to user

--- src/components/UI/Input/index.tsx
+++ src/components/UI/Input/index.tsx
@@ -1,9 +1,11 @@
 import { ForwardedRef, forwardRef } from 'react';
+import { FaCheck } from 'react-icons/fa6';
+import { VscError } from 'react-icons/vsc';
 import { InputProps } from './types';
 import { ErrorMessage } from '../ErrorMessage';
 
 export const Input = forwardRef(function Input(
-  { name, label, error, ...rest }: InputProps,
+  { name, label, error, valueInput, ...rest }: InputProps,
   ref: ForwardedRef<HTMLInputElement>,
 ) {
   return (
@@ -11,13 +13,21 @@ export const Input = forwardRef(function Input(
       <label className="text-xl font-semibold" htmlFor={name}>
         {label}:
       </label>
-      <input
-        id={name}
-        name={name}
-        ref={ref}
-        {...rest}
-        className="block w-full rounded-sm border-2 border-bluishPurple bg-transparent px-2 text-lg blueTheme:border-blueTheme dark:border-white"
-      />
+      <div className="relative flex items-center">
+        <input
+          id={name}
+          name={name}
+          ref={ref}
+          {...rest}
+          className="block w-full rounded-sm border-2 border-bluishPurple bg-transparent px-2 text-lg blueTheme:border-blueTheme dark:border-white"
+        />
+        <span className="absolute right-0 mr-2">
+          {!error && valueInput && (
+            <FaCheck size={15} className="text-successColor" />
+          )}
+          {error && <VscError size={15} className="text-dangerColor" />}
+        </span>
+      </div>
       {error && <ErrorMessage error={error} />}
     </div>
   );
--- src/components/UI/Input/types.tsx
+++ src/components/UI/Input/types.tsx
@@ -3,5 +3,6 @@ import { InputHTMLAttributes } from 'react';
 export type InputProps = {
   name: string;
   label: string;
+  valueInput?: string;
   error?: string;
 } & InputHTMLAttributes<HTMLInputElement>;
