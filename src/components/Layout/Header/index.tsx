3546bdee6c52a586e15598b11554f78543aed6ff
Matheus <mf635097@gmail.com>
2024-01-09 13:28:40 -0300

    feat: add login by header

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,11 +1,20 @@
+import { useContext } from 'react';
 import { Logo } from '../../Logo';
 import { About } from '../../About';
+import { Login } from '../../Login';
+import { AuthContext } from '../../../contexts/AuthContext/AuthContext';
+import { DropDownProfile } from '../../UI/DropDownProfile';
 
 export const Header = () => {
+  const { signed } = useContext(AuthContext);
+
   return (
     <header className="flex w-full items-center justify-between px-4 py-3">
       <Logo />
-      <About />
+      <div className="flex items-center gap-2">
+        {signed ? <DropDownProfile /> : <Login />}
+        <About />
+      </div>
     </header>
   );
 };
--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,8 +1,10 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { PomodoroContext } from '../../contexts/PomodoroContext/PomodoroContext';
 
 export const Logo = () => {
   const { pomodoro } = useContext(PomodoroContext);
+  const navigate = useNavigate();
 
   const logoPath =
     pomodoro.theme === 'defaultTheme'
@@ -13,7 +15,7 @@ export const Logo = () => {
 
   return (
     <div
-      onClick={() => location.reload()}
+      onClick={() => navigate('/')}
       aria-label="The logo site"
       className="flex cursor-pointer items-center gap-2"
     >
@@ -22,7 +24,7 @@ export const Logo = () => {
         src={logoPath}
         alt="An image of the website's logo"
       />
-      <span className=" font-sans text-2xl font-bold max-[380px]:hidden">
+      <span className="font-sans text-2xl font-bold max-[490px]:hidden">
         Focus Mate
       </span>
     </div>
