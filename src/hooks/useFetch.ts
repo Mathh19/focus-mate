3e30668e6dc6a52bc0a74dfda61cbb669b84dfad
Matheus <mf635097@gmail.com>
2024-01-09 14:17:56 -0300

    refactor: remove use client from useFetch

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,5 +1,3 @@
-'use client';
-
 import { useCallback, useContext, useEffect, useReducer } from 'react';
 import { api } from '../services/api';
 import { AuthContext } from '../contexts/AuthContext/AuthContext';
