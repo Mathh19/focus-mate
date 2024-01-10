595fb75bb88382ba8e78e7cd56f84a1fb3c1e2c0
Matheus <mf635097@gmail.com>
2024-01-10 12:58:34 -0300

    fix: improve user profile visibility

--- src/components/UI/DropDownProfile/index.tsx
+++ src/components/UI/DropDownProfile/index.tsx
@@ -13,16 +13,18 @@ export const DropDownProfile = () => {
 
   return (
     <div className="flex items-center gap-2">
-      <p className="font-semibold max-[420px]:hidden">{data?.username}</p>
+      <p className="max-w-[100px] truncate text-xl font-semibold">
+        {data?.username}
+      </p>
       <div className="relative">
         <button onClick={() => setOpen(!open)} className="relative">
-          <div className="flex h-12 w-12">
+          <div className="w-1h-14 flex h-14">
             <img
               src={`${!data?.avatar ? './no-avatar.png' : `${avatar}`}`}
-              width={48}
-              height={48}
+              width={56}
+              height={56}
               alt="Profile image"
-              className="h-12 w-full rounded-full object-cover object-center p-[2px]"
+              className="h-14 w-full rounded-full object-cover object-center p-[2px]"
             />
           </div>
           <div className="absolute inset-0 -z-10 h-full w-full rounded-full bg-gradient-to-b from-bluishPurple from-10% to-pink blueTheme:from-blueTheme blueTheme:to-blueTheme-light-blue dark:from-white dark:to-white"></div>
